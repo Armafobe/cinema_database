214a73b20fb3b447788b683bda89c7b34af21ff6
Armafobe <93781301+Armafobe@users.noreply.github.com>
2022-06-16 16:50:09 +0200

    Create values.sql

---
+++ values.sql
@@ -0,0 +1,6 @@
+INSERT INTO admin (username, password) VALUES 
+('lsewill0', '$2y$10$4A3LYXJ39OSZK96BnUx07uQXmhG98GB3sbEyWxWQOmuxOFsJ5Eed'),
+('zblaylock1', '$2y$10$LUDtIo/Lk34XXPQGxAYn0OloHabgkydgGaY54gMFjCZFNdA8tF83i'),
+('rdovermann2', '$2y$10$FnmJKZYVMo11OWmm48gb3OWzaqLLeiS9fn2rnusv1a3mvszdlEWUu'),
+('rdabels3', '$2y$10$MMeKx8tzdOBoSsJXBVyXU.64/ixpwdnR8l/A8Qslq0azb16R15Z86'),
+('gstoller4', '$2y$10$PBXadQd7Ss6xpsRMN3zg1epCzwzBmpVyWJeoEtCSHKkYRz2jIKGNu');
