e446490658ba047698485d58f22069dad6dbe3f6
Armafobe <93781301+Armafobe@users.noreply.github.com>
2022-06-16 17:21:23 +0200

    Update values.sql

--- values.sql
+++ values.sql
@@ -11,3 +11,12 @@ INSERT INTO user (username, password) VALUES
 ('kfoddy2', '$2y$10$K5GdJIexEJgY3wxfm8FHH.WCZ7VWCOEHW.LewzJuOuy3eNnF9qrUm'),
 ('nbechley3', '$2y$10$EfU1BU3wC2deLT9B78dXl.zA.6WEBpeXkJYcvWcDsfRVd8lYXStNa'),
 ('deilhart4', '$2y$10$Mopa7UkmF5LvS1O1J/8eMOmJ6iSctHl8zjh0SP51BT5kI3RzfeIU.');
+
+INSERT INTO movie_theater (name, city, address, zipcode, user_id) VALUES
+('Cinémarine', 'Saint-Gilles-Croix-de-Vie', 'Rue de la Félicite', 85800, 1),
+('Gaumont', 'Nantes', '12 Place du Commerce', 44000, 2),
+('Le Royal', 'Biarritz', '8 Avenue du Maréchal Foch', 64200, 3),
+('Pathé', 'Annecy', '7 Avenue de Brogny', 74000, 3),
+('Olympia', 'Cannes', '5 Rue de la Pompe', 06400, 4),
+('UGC Ciné Cité Bercy', 'Paris', '2 Cour Saint-Emilion', 75012, 5),
+('UGC Ciné Cité Strasbourg Etoile', 'Strasbourg', '25 Avenue du Rhin', 67100, 5);
