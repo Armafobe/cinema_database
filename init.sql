a25285c6503f492f2c5e0019d010593f015f45d3
Armafobe <93781301+Armafobe@users.noreply.github.com>
2022-06-16 14:40:40 +0200

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,6 @@
+-- Installer MYSQL Server, Router, Workbench & Shell
+-- Ouvrir MySQL Workbench
+
+-- Créer la base de données
+
+CREATE DATABASE IF NOT EXISTS cinema_booking CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
