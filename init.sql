9722f63d56636084fbf458f859f1e04592998335
Armafobe <93781301+Armafobe@users.noreply.github.com>
2022-06-16 15:20:12 +0200

    Update init.sql

--- init.sql
+++ init.sql
@@ -28,3 +28,11 @@ CREATE TABLE IF NOT EXISTS movie_theater(
 	user_id INT(11) NOT NULL,
 	CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES user(id)
 ) ENGINE=InnoDB; 
+
+CREATE TABLE IF NOT EXISTS movie_theater_admin(
+	admin_id INT(11) NOT NULL,
+	movie_theater_id INT(11) NOT NULL,
+	PRIMARY KEY (admin_id, movie_theater_id),
+	CONSTRAINT FK_admin_id FOREIGN KEY (admin_id) REFERENCES admin(id) ON DELETE CASCADE,
+	CONSTRAINT FK_movie_theater FOREIGN KEY (movie_theater_id) REFERENCES movie_theater(id)
+) ENGINE=InnoDB;
