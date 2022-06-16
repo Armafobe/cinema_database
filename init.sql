cc393ea63fcc28e44219fc40d93b409a4119b93b
Armafobe <93781301+Armafobe@users.noreply.github.com>
2022-06-16 16:40:40 +0200

    Update init.sql

--- init.sql
+++ init.sql
@@ -69,3 +69,30 @@ CREATE TABLE IF NOT EXISTS movie_room_session (
 	CONSTRAINT FK_movie_room_id FOREIGN KEY (movie_room_id) REFERENCES movie_room(id) ON DELETE CASCADE,
 	CONSTRAINT FK_movie_session_id FOREIGN KEY (movie_session_id) REFERENCES movie_session(id)
 ) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS payment (
+	id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	type VARCHAR(30) NOT NULL,
+	price DECIMAL(4, 2) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS customer (
+	id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	lastname VARCHAR(60) NOT NULL,
+	firstname VARCHAR(60) NOT NULL,
+	username VARCHAR(60) UNIQUE,
+	password CHAR(255) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS booking (
+	id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	booking_date DATETIME NOT NULL,
+	payment_type VARCHAR(20) NOT NULL,
+	payment_date DATETIME NOT NULL,
+	movie_session_id_booking INT(11) NOT NULL,
+	payment_id INT(11) NOT NULL,
+	customer_id INT(11) NOT NULL,
+	CONSTRAINT FK_movie_session_id_booking FOREIGN KEY (movie_session_id_booking) REFERENCES movie_session(id),
+	CONSTRAINT FK_payment_id FOREIGN KEY (payment_id) REFERENCES payment(id),
+	CONSTRAINT FK_customer_id FOREIGN KEY (customer_id) REFERENCES customer(id)
+) ENGINE=InnoDB;
